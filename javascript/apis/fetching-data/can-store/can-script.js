5a731618d4fdc852fa51ae65779f430dae460d45
mckelveygreg <mckelveygreg@gmail.com>
2018-03-20 21:28:36 -0700

    XHR Can-script redo

--- javascript/apis/fetching-data/can-store/can-script.js
+++ javascript/apis/fetching-data/can-store/can-script.js
@@ -1,19 +1,41 @@
 // create a variable to store the products 'database' in
 var products;
-
+var url = 'products.json';
 // use fetch to retrieve it, and report any errors that occur in the fetch operation
 // once the products have been successfully loaded and formatted as a JSON object
 // using response.json(), run the initialize() function
-fetch('products.json').then(function(response) {
-  if(response.ok) {
-    response.json().then(function(json) {
-      products = json;
-      initialize();
-    });
-  } else {
-    console.log('Network request for products.json failed with response ' + response.status + ': ' + response.statusText);
-  }
-});
+
+function xhrVersion() {
+
+	var request = new XMLHttpRequest();
+	request.open('GET', 'products.json');
+	request.responseType = 'json';
+
+	request.onload = function () {
+		if (request.status === 200) {
+			products = request.response;
+			console.log('did i make it to onload');
+			initialize();
+		} else {
+			console.log('Network request for products.json failed with response ' + request.status + ': ' + request.statusText);
+		}
+	};
+	request.send();
+}
+	
+
+xhrVersion();
+
+//fetch('products.json').then(function (response) {
+//  if(response.ok) {
+//    response.json().then(function(json) {
+//      products = json;
+//      initialize();
+//    });
+//  } else {
+//    console.log('network request for products.json failed with response ' + response.status + ': ' + response.statustext);
+//  }
+//});
 
 // sets up the app logic, declares required variables, contains all the other functions
 function initialize() {
