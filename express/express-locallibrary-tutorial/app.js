34cd926ceb0c2116f21308ee4deb996a7f4f23e5
mckelveygreg <mckelveygreg@gmail.com>
2018-06-17 09:14:27 -0700

    routes skeleton setup

--- express/express-locallibrary-tutorial/app.js
+++ express/express-locallibrary-tutorial/app.js
@@ -14,6 +14,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog'); // imports routes from "catalog" area of site
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter); // Add catalog router to middleware chain
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
