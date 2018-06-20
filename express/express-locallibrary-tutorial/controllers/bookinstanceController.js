b105353c551f68a866e6189cc89283a4d1972519
mckelveygreg <mckelveygreg@gmail.com>
2018-06-20 09:43:53 -0700

    Delete bookinstance challenge!

--- express/express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ express/express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -97,13 +97,28 @@ exports.bookinstance_create_post = [
 ];
 
 // Display BookInstance delete form on GET.
-exports.bookinstance_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+exports.bookinstance_delete_get = function(req, res, next) {
+    BookInstance.findById(req.params.id)
+    .exec(function (err, bookinstance) {
+        if (err) { return next(err); }
+        // Successful, so render.
+        res.render('bookinstance_delete', {title: 'Delete Book Instance', bookinstance: bookinstance});
+    });
+
 };
 
 // Handle BookInstance delete on POST.
-exports.bookinstance_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete POST');
+exports.bookinstance_delete_post = function(req, res, next) {
+    BookInstance.findById(req.body.bookinstanceid)
+    .exec(function (err, bookinstance) {
+        if (err) { return next(err); }
+        //Successful, so delete! Muhahahah
+        BookInstance.findByIdAndRemove(req.body.bookinstanceid, function deleteBookInstance(err) {
+            if (err) { return next(err); }
+            // Success - go to bookinstance list
+            res.redirect('/catalog/bookinstances')
+        })
+    });
 };
 
 // Display BookInstance update form on GET.
